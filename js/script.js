5ab318c1fcfca089dab648146a8605c401f66845
nnhao280501 <nnhao280501@gmail.com>
2022-06-08 07:30:59 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Graphic Designer", "Web Designer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
+let navtoggle = document.querySelector('.aside');
+document.querySelector('.nav-toggler ').onclick = () => {
+    navtoggle.classList.toggle('active')
+}
